8fb28dfe30e0ba78fb995863f826d17fd1d23bc5
Adesugba Samuel <97664905+Adesugba-Samuel@users.noreply.github.com>
2022-10-01 22:46:23 +0100

    Add files via upload

---
+++ DTUITION/main.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.site-content');
+ScrollReveal().reveal('.col-md-4');
+ScrollReveal().reveal('.d-flex');
+ScrollReveal().reveal('.col-md-7');
+ScrollReveal().reveal('.col-md-5');
+ScrollReveal().reveal('.card');
+
+// All animations will take exactly 500ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 500,
+	speedAsDuration: true
+});
